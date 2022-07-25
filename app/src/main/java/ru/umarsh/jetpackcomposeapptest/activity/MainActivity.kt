5aef74b1b56a2c5d7a794191029214b5286e5e2b
Vycheslav Tumanov <tumanov@umarsh.ru>
2022-07-25 21:31:07 +0300

    add image card

--- app/src/main/java/ru/umarsh/jetpackcomposeapptest/activity/MainActivity.kt
+++ app/src/main/java/ru/umarsh/jetpackcomposeapptest/activity/MainActivity.kt
@@ -3,29 +3,38 @@ package ru.umarsh.jetpackcomposeapptest.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import ru.umarsh.jetpackcomposeapptest.ui.ComposeTheme
+import androidx.compose.ui.unit.sp
+import ru.umarsh.jetpackcomposeapptest.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting(name = "Slava")
-                }
+            val painter = painterResource(id = R.drawable.cat)
+            val description = "Котенок на траве"
+            val title = "Котенок на траве"
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
         }
 
@@ -33,69 +42,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxHeight(0.9f)
-                .background(Color.Green),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceAround
-        )
-        {
-            Text(text = "Hello $name!")
-            Text(text = "Hello World!")
-            Text(text = "Jetpack")
-        }
-        Column(
-            modifier = Modifier
-                .width(200.dp)
-                .height(300.dp)
-                .background(Color.Gray)
-                .border(5.dp, Color.Magenta)
-                .padding(5.dp)
-                .border(5.dp, Color.Red)
-                .padding(5.dp)
-                .border(5.dp, Color.Cyan),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceAround
-        )
-        {
-            Text(text = "Hello $name!")
-            Text(text = "Hello World!")
-            Text(text = "Jetpack")
-        }
-        Column(
-            modifier = Modifier
-                .background(Color.Green)
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            Text(text = "Hello", modifier = Modifier.clickable {
-                
-            })
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(
-                text = "World!", modifier = Modifier
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .offset(10.dp, 10.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(5.dp)
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
             )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting(name = "Vyacheslav")
     }
 }
