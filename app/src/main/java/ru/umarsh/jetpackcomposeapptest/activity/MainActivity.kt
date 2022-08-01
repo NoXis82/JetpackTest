2d251aa0dcd82296148c4f92bab5141717275605
Vycheslav Tumanov <tumanov@umarsh.ru>
2022-08-01 22:31:38 +0300

    state

--- app/src/main/java/ru/umarsh/jetpackcomposeapptest/activity/MainActivity.kt
+++ app/src/main/java/ru/umarsh/jetpackcomposeapptest/activity/MainActivity.kt
@@ -4,70 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import ru.umarsh.jetpackcomposeapptest.ui.ComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting(name = "Slava")
+
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
                 }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
             }
         }
-
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Row( modifier = Modifier
-        .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically) {
-        Column(
-            modifier = Modifier
-                .fillMaxHeight(0.9f)
-                .background(Color.Green),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceAround
-        )
-        {
-            Text(text = "Hello $name!")
-            Text(text = "Hello World!")
-            Text(text = "Jetpack")
-        }
-        Column(
-            modifier = Modifier
-                .width(200.dp)
-                .height(300.dp)
-                .background(Color.Gray),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceAround
-        )
-        {
-            Text(text = "Hello $name!")
-            Text(text = "Hello World!")
-            Text(text = "Jetpack")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting(name = "Vyacheslav")
-    }
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        })
 }
