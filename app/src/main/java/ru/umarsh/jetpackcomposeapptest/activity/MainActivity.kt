6f8bdf53e47557943e9f3c047be11d99456b7266
Vycheslav Tumanov <tumanov@umarsh.ru>
2022-09-26 12:12:01 +0300

    ui_witch_jc

--- app/src/main/java/ru/umarsh/jetpackcomposeapptest/activity/MainActivity.kt
+++ app/src/main/java/ru/umarsh/jetpackcomposeapptest/activity/MainActivity.kt
@@ -3,12 +3,16 @@ package ru.umarsh.jetpackcomposeapptest.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import ru.umarsh.jetpackcomposeapptest.ui.HomeScreen
+import ru.umarsh.jetpackcomposeapptest.ui.MeditationUIYouTubeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MeditationUIYouTubeTheme {
+                HomeScreen()
+            }
         }
     }
 }
---
+++ app/src/main/java/ru/umarsh/jetpackcomposeapptest/data/BottomMenuContent.kt
@@ -0,0 +1,8 @@
+package ru.umarsh.jetpackcomposeapptest.data
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
