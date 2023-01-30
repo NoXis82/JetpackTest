15b0542e5d3d6a7facfafe581d018131cf391d77
Vycheslav Tumanov <tumanov@umarsh.ru>
2023-01-30 18:06:45 +0300

    buffer, conflate, collectLatest

--- app/src/main/java/ru/umarsh/jetpackcomposeapptest/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/umarsh/jetpackcomposeapptest/viewmodel/MainViewModel.kt
@@ -3,7 +3,10 @@ package ru.umarsh.jetpackcomposeapptest.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
@@ -24,10 +27,36 @@ class MainViewModel : ViewModel() {
     }
 
     private fun collectionFlow() {
+        val flow = flow {
+            delay(250L)
+            emit("Закуски")
+            delay(1000L)
+            emit("Основное блюдо")
+            delay(100L)
+            emit("Десерт")
+        }
         viewModelScope.launch {
-            countDownFlow.collect {
-                println("Current time: $it")
+            flow.onEach {
+                println("FLOW: $it доставлен")
             }
+                .buffer()
+               // .conflate()
+                .collect {
+                    println("FLOW: Сейчас едим: $it")
+                    delay(1500L)
+                    println("FLOW: Закончили есть: $it")
+                }
+
+//            countDownFlow
+//                .filter {
+//                    it % 2 == 0
+//                }
+//                .map {
+//                    it * it
+//                }
+//                .collect {
+//                println("Current time: $it")
+//            }
         }
     }
 
